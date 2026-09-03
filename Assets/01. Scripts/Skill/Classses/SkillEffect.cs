7cf2bc84bf2d45b45343341d6e8abdc861399172
tonyxhsl <kimtaehyun258@gmail.com>
2026-09-03 19:58:37 +0900

    feat: 스킬 수치 계산 시 다양한 계수 사용 가능하도록 수정

--- Assets/01. Scripts/Skill/Classses/SkillEffect.cs
+++ Assets/01. Scripts/Skill/Classses/SkillEffect.cs
@@ -1,9 +1,13 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 // <summary>
 // 스킬이 대상에게 적용하는 개별 효과
 // 데미지, 회복, 버프, 디버프, 이동 등을 정의
+// 스킬의 최종 수치는 스킬의 기본 수치 + 스탯 계수에 따른 수치로 계산됨
+// ex1) 공격 데미지 = 스킬 기본 수치 5 + 공격력 * 0.5
+// ex2) 보호막량 = 스킬 기본 수치 10 + 방어력 * 0.3
 // </summary>
 
 public enum SkillEffectType
@@ -20,7 +24,7 @@ public enum SkillEffectTarget
     Self        // 시전자 자신
 }
 
-public enum SkillEffectStat
+public enum StatType
 {
     None,
     MaxHp,
@@ -33,6 +37,18 @@ public enum SkillEffectStat
 }
 
 [Serializable]
+public class StatScaling
+{
+    [Tooltip("계수에 사용할 스탯")]
+    public StatType statType;
+
+    [Tooltip("스탯 계수. 1 = 해당 스탯 100%")]
+    [Min(0f)]
+    public float ratio;
+}
+
+
+[Serializable]
 public class SkillEffect
 {
     [Header("Effect")]
@@ -40,12 +56,11 @@ public class SkillEffect
     public SkillEffectTarget effectTarget;
 
     [Header("Value")]
-    [Tooltip("고정 수치")]
-    public int amount;
+    [Tooltip("기본 수치")]
+    public int baseAmount;
 
-    [Tooltip("공격력 계수. 1 = 공격력 100%")]
-    [Min(0f)]
-    public float attackRatio;
+    [Tooltip("효과 수치에 반영할 스탯 계수 목록")]
+    public List<StatScaling> statScalings = new();
 
     [Header("Status Effect")]
     [Tooltip("적용할 상태효과. StatusEffect일 때만 사용")]
