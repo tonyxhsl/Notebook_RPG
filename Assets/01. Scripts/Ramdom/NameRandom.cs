ae9f47dc6b0da4aaea58b7addc0f31a3c717d3e8
tonyxhsl <kimtaehyun258@gmail.com>
2026-09-12 01:00:42 +0900

    chore: NameRandom 스크립트 주석 수정

--- Assets/01. Scripts/Ramdom/NameRandom.cs
+++ Assets/01. Scripts/Ramdom/NameRandom.cs
@@ -1,10 +1,10 @@
-//Todo: 성별에 따라 주어진 후보군에서 이름을 랜덤으로 가지는 클래스
-// 2개의 리스트 : 남자 후보군 & 여자 후보군
-// GetValue함수 : 성별에 따른 리스트에서 무작위로 하나의 이름을 반환
-// Tip: SpriteRandom 참고
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+// <summary>
+// 성별에 따라 주어진 후보군에서 이름을 랜덤으로 가지는 클래스
+// </summary>
 
 [Serializable]
 public class NameRandom
